a90df5b64d6cd5319324fb0d03a1f0f048820239
N-eeraj <neeraj7rajeena@gmail.com>
2022-07-26 23:41:19 +0530

    Initial Commit: Partial basic project set up

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from './js/App.vue'
+import router from './js/router'
+import store from './js/store'
 
 createApp(App).use(store).use(router).mount('#app')
