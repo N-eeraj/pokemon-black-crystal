f6c4d040be7bc555a653d0a2ca912e4e8baa06ae
N-eeraj <neeraj7rajeena@gmail.com>
2022-07-27 07:36:51 +0530

    SCSS changes & added media queries for landscape & rotate device

--- src/js/App.vue
+++ src/js/App.vue
@@ -1,5 +1,8 @@
 <template>
   <router-view/>
+  <div id="rotate_device" style="background-color: $white">
+    Please rotate your device
+  </div>
 </template>
 
 <style lang="scss" src="../styles/app.scss"></style>
