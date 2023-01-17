6d52cd7a834f61c5bd2b348cbe12a8affa805477
N-eeraj <neeraj7rajeena@gmail.com>
2023-01-17 07:17:24 +0530

    Loaded gym badges details

---
+++ src/js/views/GymBadges.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <navigation-bar
+            title="Badges"
+            icon="cross-mark"
+            @iconEvent="$router.push('/')" />
+        <img
+            v-for="(badge, index) in badges"
+            :src="require(`@/assets/images/gym-badges/${badge.name}.svg`)"
+            :key="index" />
+    </div>
+</template>
+
+<script>
+
+    import NavigationBar from "@/js/components/UI/NavigationBar.vue"
+
+    import data from "@/assets/data/gym-levels.json"
+
+    export default {
+        name: 'gym-badges',
+
+        components: {
+            NavigationBar
+        },
+
+        data() {
+            return {
+                badges: null
+            }
+        },
+
+        created() {
+            this.badges = data
+        },
+    }
+
+</script>
