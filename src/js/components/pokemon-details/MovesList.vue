0e3a61fe973fd16a8ec8afdf61b4e255902c3051
N-eeraj <neeraj7rajeena@gmail.com>
2023-07-13 14:07:24 +0530

    Basic setup for moves list in pokemon details view

---
+++ src/js/components/pokemon-details/MovesList.vue
@@ -0,0 +1,15 @@
+<template>
+    {{ moves }}
+</template>
+
+<script>
+export default {
+    name: 'moves-list',
+    props: {
+        moves: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
