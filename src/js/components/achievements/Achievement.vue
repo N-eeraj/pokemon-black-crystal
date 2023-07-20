62e4cfdd86eb50233e6eb63397614a8768966a4b
N-eeraj <neeraj7rajeena@gmail.com>
2023-07-20 22:20:37 +0530

    Basic setup for achievement update

---
+++ src/js/components/achievements/Achievement.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="achievement-card">
+        {{ name }}
+        {{ badge }}
+        {{ required }}
+        {{ current }}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'achievement',
+
+        props: {
+            name: {
+                type: String,
+                required: true
+            },
+            badge: {
+                type: String,
+                required: true
+            },
+            required: {
+                type: Array,
+                required: true
+            },
+            current: {
+                type: Number,
+                required: false,
+                default: 0
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped src="@/styles/home/achievements/item.scss"></style>
---
+++ src/js/components/achievements/AchievementsGroup.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="achievements-group">
+        <strong>
+            {{ title }}
+        </strong>
+        <div class="achievements-container">
+            <achievement
+                v-for="({name, badge, required, current}, index) in achievements"
+                :name="name"
+                :badge="badge"
+                :required="required"
+                :current="current"
+                :key="index" />
+        </div>
+    </div>
+</template>
+
+<script>
+    import Achievement from '@/js/components/achievements/Achievement.vue'
+
+    export default {
+        name: 'achievements-group',
+
+        components: {
+            Achievement
+        },
+
+        props: {
+            title: {
+                type: String,
+                required: true
+            },
+            achievements: {
+                type: Array,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped src="@/styles/home/achievements/group.scss"></style>
