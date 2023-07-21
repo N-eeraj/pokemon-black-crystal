f31d453255fe957527f84f9f9ea3500765558bb5
N-eeraj <neeraj7rajeena@gmail.com>
2023-07-22 01:46:13 +0530

    Added regional owned pokemon achievements

--- src/assets/data/achievements/index.js
+++ src/assets/data/achievements/index.js
@@ -1,7 +1,9 @@
 import arcade from '@/assets/data/achievements/arcade'
 import pokemonOwned from '@/assets/data/achievements/pokemonOwned'
+import region from '@/assets/data/achievements/region'
 
 export default [
     arcade,
-    pokemonOwned
+    pokemonOwned,
+    region
 ]
