4d669c33a85a8fb8b79b16bee3316a22d51fabb5
N-eeraj <neeraj7rajeena@gmail.com>
2023-07-23 12:16:55 +0530

    Added story achievements

--- src/assets/data/achievements/index.js
+++ src/assets/data/achievements/index.js
@@ -1,9 +1,11 @@
 import arcade from '@/assets/data/achievements/arcade'
 import pokemonOwned from '@/assets/data/achievements/pokemonOwned'
 import region from '@/assets/data/achievements/region'
+import story from '@/assets/data/achievements/story'
 
 export default [
     arcade,
     pokemonOwned,
-    region
+    region,
+    story
 ]
