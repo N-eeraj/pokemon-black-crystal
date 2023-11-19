a8e0f9d6777e671e247a4ff063c6b2d11c036beb
N-eeraj <neeraj7rajeena@gmail.com>
2023-11-19 14:30:12 +0530

    Migration for duplicate pokemon evolution data

--- src/js/mixins/migrations/index.js
+++ src/js/mixins/migrations/index.js
@@ -9,6 +9,7 @@ import migration_v_1_9_7 from '@/js/mixins/migrations/migration_v_1_9_7'
 import migration_v_2_0_0 from '@/js/mixins/migrations/migration_v_2_0_0'
 import migration_v_2_1_1 from '@/js/mixins/migrations/migration_v_2_1_1'
 import migration_v_2_1_2 from '@/js/mixins/migrations/migration_v_2_1_2'
+import migration_v_2_2_1 from '@/js/mixins/migrations/migration_v_2_2_1'
 
 
 const updateVersion = () => {
@@ -21,6 +22,7 @@ const updateVersion = () => {
     migration_v_2_0_0(currentVersion)
     migration_v_2_1_1(currentVersion)
     migration_v_2_1_2(currentVersion)
+    migration_v_2_2_1(currentVersion)
 }
 
 export default updateVersion
