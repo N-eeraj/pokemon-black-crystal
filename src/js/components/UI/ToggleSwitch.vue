b3e8c48b574cb80bb073e26ec37b46e9dda29e79
N-eeraj <neeraj7rajeena@gmail.com>
2024-01-29 21:33:30 +0530

    Added switch component

---
+++ src/js/components/UI/ToggleSwitch.vue
@@ -0,0 +1,23 @@
+<template>
+	<label class="switch" :class="{active: modelValue}">
+		<input
+			:value="modelValue"
+			type="checkbox"
+			@change="$emit('update:modelValue', !modelValue)" />
+	</label>
+</template>
+
+<script>
+	export default {
+		name: 'toggle-switch',
+		props: {
+			modelValue: {
+				type: Boolean,
+				required: false,
+				default: false
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped src="@/styles/UI/switch.scss"></style>
--- src/js/views/SettingsView.vue
+++ src/js/views/SettingsView.vue
@@ -1,9 +1,47 @@
 <template>
     <div>
+        <div id="settings">
+            <navigation-bar
+                title="Settings"
+                icon="cross-mark"
+                @icon-event="$router.push('/')" />
+
+            <div class="options">
+                <label>
+                    <span>
+                        Sound
+                    </span>
+                    <toggle-switch v-model="soundStatus" @change="toggleSound" />
+                </label>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import NavigationBar from '@/js/components/UI/NavigationBar.vue'
+    import ToggleSwitch from '@/js/components/UI/ToggleSwitch.vue'
+
+    import { mapGetters, mapActions } from 'vuex'
+
+    export default {
+    name: 'settings-view',
+
+        components: {
+            NavigationBar,
+            ToggleSwitch
+        },
+
+        computed: {
+            ...mapGetters(['soundStatus'])
+        },
+
+        methods: {
+            ...mapActions([
+                'toggleSound'
+            ])
+        }
+    }
 </script>
 
 <style lang="scss" scoped src="@/styles/settings.scss"></style>
