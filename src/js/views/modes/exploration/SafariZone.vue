ff31d3264872eda3daae131ce6b6a81198009ccb
N-eeraj <neeraj7rajeena@gmail.com>
2024-05-07 10:45:36 +0530

    Tall grass generation & interaction

--- src/js/views/modes/exploration/SafariZone.vue
+++ src/js/views/modes/exploration/SafariZone.vue
@@ -38,6 +38,12 @@
                 &rarr;
             </button>
         </div>
+
+        <div class="sprites">
+            <img
+                ref="tallGrassImage"
+                src="@/assets/images/safari-zone/tall-grass.png" />
+        </div>
     </div>
 </template>
 
@@ -53,16 +59,18 @@
                 ctx: null,
                 game: {
                     size: null,
+                    tile: 0,
+                    pokemon: null
                 },
                 player: {
                     position: {
                         x: 0,
                         y: 0,
                     },
-                    size: 0,
                     direction: null
                 },
-                tallGrasses: []
+                tallGrasses: [],
+                tallGrassImage: null
             }
         },
 
@@ -88,13 +96,12 @@
             canvas.width = clientWidth
             canvas.height = clientHeight
             this.ctx = canvas.getContext('2d')
-            this.player.size = clientWidth / 8
-            this.player.position.x = (clientWidth - this.player.size) / 2
-            this.player.position.y = (clientHeight - this.player.size) / 2
+            this.initZone()
 
             const animate = () => {
                 this.draw()
-                requestAnimationFrame(animate)
+                if (!this.game.pokemon)
+                    requestAnimationFrame(animate)
             }
 
             animate()
@@ -103,6 +110,44 @@
         },
 
         methods: {
+            grassIntersection(x, y) {
+                let grassLeft = x * this.game.tile
+                let grassRight = (x + 1) * this.game.tile
+                let grassUp = y * this.game.tile
+                let grassDown = (y + 1) * this.game.tile
+                const intersectionX = grassLeft <= this.player.position.x && grassRight >= this.player.position.x || grassLeft <= this.player.position.x + this.game.tile && grassRight >= this.player.position.x + this.game.tile
+                const intersectionY = grassUp <= this.player.position.y && grassDown >= this.player.position.y || grassUp <= this.player.position.y + this.game.tile && grassDown >= this.player.position.y + this.game.tile
+                return intersectionX && intersectionY
+            },
+
+            initZone() {
+                this.game.tile = this.game.size.width / 8
+                this.player.position.x = (this.game.size.width - this.game.tile) / 2
+                this.player.position.y = (this.game.size.height - this.game.tile) / 2
+
+                for (let i = 0; i < 80; i++) {
+                    const setGrassPosition = () => {
+                        let xPosition = Math.floor(Math.random() * this.game.size.width / this.game.tile)
+                        let yPosition = Math.floor(Math.random() * this.game.size.height / this.game.tile)
+                        
+                        const playerOverlap = this.grassIntersection(xPosition, yPosition)
+                        if (this.tallGrasses.some(({ x, y }) => x === xPosition && y === yPosition) || playerOverlap) {
+                            return setGrassPosition()
+                        }
+                        else {
+                            return [ xPosition, yPosition ]
+                        }
+                    }
+                    const [ x, y ] = setGrassPosition()
+
+                    this.tallGrasses.push({
+                        x,
+                        y,
+                        pokemon: Math.random() > 0.8 ? 2 : null,
+                    })
+                }
+            },
+
             handleKeyboard({ key }) {
                 switch (key.toLowerCase()) {
                     case 'w':
@@ -125,7 +170,6 @@
             },
 
             drawPlayer() {
-                this.ctx.fillStyle = 'black'
                 switch (this.player.direction) {
                     case 'up':
                         this.player.position.y -= 3
@@ -142,19 +186,37 @@
                 }
                 if (this.player.position.y < 10)
                     this.player.position.y = 10
-                else if (this.player.position.y + this.player.size > this.game.size.height - 10)
-                    this.player.position.y = this.game.size.height - this.player.size - 10
+                else if (this.player.position.y + this.game.tile > this.game.size.height - 10)
+                    this.player.position.y = this.game.size.height - this.game.tile - 10
                 if (this.player.position.x < 10)
                     this.player.position.x = 10
-                else if (this.player.position.x + this.player.size > this.game.size.width - 10)
-                    this.player.position.x = this.game.size.width - this.player.size - 10
-                this.ctx.fillRect(this.player.position.x, this.player.position.y, this.player.size, this.player.size)
+                else if (this.player.position.x + this.game.tile > this.game.size.width - 10)
+                    this.player.position.x = this.game.size.width - this.game.tile - 10
+                this.ctx.fillStyle = 'black'
+                this.ctx.fillRect(this.player.position.x, this.player.position.y, this.game.tile, this.game.tile)
+            },
+
+            drawTallGrasses() {
+                const image = this.$refs.tallGrassImage
+                this.tallGrasses.forEach(({ x, y, pokemon }) => {
+                    if (this.grassIntersection(x, y))
+                        this.game.pokemon = pokemon
+                    this.ctx.drawImage(image, x * this.game.tile, y * this.game.tile, this.game.tile, this.game.tile)
+                })
+                // debug option
+                this.tallGrasses.forEach(({ x, y, pokemon }) => {
+                    if (pokemon) {
+                        this.ctx.strokeStyle = this.grassIntersection(x, y) ? 'red' : 'black'
+                        this.ctx.strokeRect(x * this.game.tile, y * this.game.tile, this.game.tile, this.game.tile)
+                    }
+                })
             },
 
             draw() {
                 this.ctx.clearRect(0, 0, this.game.size.width, this.game.size.height)
                 this.ctx.fillStyle = '#78c9a6'
                 this.ctx.fillRect(0, 0, this.game.size.width, this.game.size.height)
+                this.drawTallGrasses()
                 this.drawPlayer()
             }
         },
